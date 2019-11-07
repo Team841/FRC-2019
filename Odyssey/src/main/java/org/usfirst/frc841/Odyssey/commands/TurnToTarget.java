22f808cb2929d6bb6c32f6102f9719990e6b2a7f
Team841 <josh.quintero@gmail.com>
2019-11-06 19:23:14 -0800

    Fixed networkTables, method to Command
    Fixed networkTables
    -Variables declared at Class level
    -Updates to variables are made in Periodic method
    -Updates are posted to private variables in drivetrain class
    -networkTables updates were removed from the turntotarget function (no longer crashes the code yay)
    
    Turn to target Command updated so that it actually calls the turntotarget method from the drivetrain instead of doing nothing

--- Odyssey/src/main/java/org/usfirst/frc841/Odyssey/commands/TurnToTarget.java
+++ Odyssey/src/main/java/org/usfirst/frc841/Odyssey/commands/TurnToTarget.java
@@ -43,6 +43,7 @@ public class TurnToTarget extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    Robot.driveTrain.TurnToTarget();
     }
 
     // Make this return true when this Command no longer needs to run execute()
